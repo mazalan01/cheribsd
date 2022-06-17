paulmetzger/pffm2-cheribsd-fork

Opportunistically colocate within login session
Previously we've tried to colocate processes with their terminal
session leaders (the setsid(2) ones).  Try to colocate with the
parent instead, except after calling setlogin(2).

Generally speaking, we can opportunisticaly colocate wherever
we want; the worst thing that can happen is that one process
will DoS others by exhausting the address space.  Session IDs
were convenient, but for capv we want to colocate as much as
possible, because otherwise we'll start with an empty capability
vector (and thus having to manually recreate it) a bit too often.

This change results in a pretty much single address space
per login session.

sys/sys/proc.h
@@ -838,6 +838,7 @@ struct proc {
 #define	P2_STKGAP_DISABLE_EXEC	0x00001000	/* Stack gap disabled
 						   after exec */
 #define	P2_ITSTOPPED		0x00002000
+#define	P2_NOCOLOCATE		0x00004000
 
 /* Flags protected by proctree_lock, kept in p_treeflags. */
 #define	P_TREE_ORPHANED		0x00000001	/* Reparented, on orphan list */

mazalan01/cheribsd

sys/sys/proc.h
@@ -856,6 +856,7 @@ struct proc {
 #define	P2_NO_NEW_PRIVS		0x00008000	/* Ignore setuid */
 #define	P2_WXORX_DISABLE	0x00010000	/* WX mappings enabled */
 #define	P2_WXORX_ENABLE_EXEC	0x00020000	/* WXORX enabled after exec */
+#define	P2_NOCOLOCATE		0x00040000
 
 /* Flags protected by proctree_lock, kept in p_treeflags. */
 #define	P_TREE_ORPHANED		0x00000001	/* Reparented, on orphan list */